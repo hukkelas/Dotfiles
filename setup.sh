4eb99ecf58c260a361abffa6ad78150fe5ee40ed
Håkon Hukkelås <hakon.hukkelas@ntnu.no>
2023-12-29 09:00:28 +0100

    update setup script

--- setup.sh
+++ setup.sh
@@ -4,24 +4,31 @@ if [[ "$OSTYPE" == "darwin"* ]]; then
   brew install fzf
   /opt/homebrew/opt/fzf/install --no-bash
 else
-    sudo apt install neovim
-    fzf_dir=~/.fzf
-    if [[! -d $fzf_dir]]; then
-        git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf && cd ~/.fzf/ && ./install
-    fi
+  apt update && apt install neovim
+
+  # Check if fzf is already installed
+  fzf_dir=~/.fzf
+  if [[ ! -d $fzf_dir ]]; then
+    git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
+    ~/.fzf/install
+  else
+    echo "The folder $fzf_dir already exists"
+  fi
 fi
 
+# Create symbolic links for configuration files
 ln -s "$HOME/.dotfiles/zsh/.zshenv" "$HOME/.zshenv"
 ln -s "$HOME/.dotfiles/tmux/.tmux.conf" "$HOME/.tmux.conf"
 
-# TPM
+# Install Tmux Plugin Manager (TPM)
 tpm_dir=~/.tmux/plugins/tpm
 if [[ ! -d $tpm_dir ]]; then
-    git clone https://github.com/tmux-plugins/tpm $tpm_dir
-    echo "Cloned TPM plugin into $tpm_dir"
+  git clone https://github.com/tmux-plugins/tpm $tpm_dir
+  echo "Cloned TPM plugin into $tpm_dir"
 else
-    echo "The folder $tpm_dir already exists"
+  echo "The folder $tpm_dir already exists"
 fi
+
 # Confirm the action with the user
 read -p "Are you sure you want to set Zsh as the default shell? (y/n): " choice
 
@@ -38,4 +45,3 @@ if [[ "$choice" =~ ^[Yy]$ ]]; then
 else
   echo "Operation canceled."
 fi
-
